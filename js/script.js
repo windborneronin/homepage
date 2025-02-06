051758fc1550bf70442a062e50abe5fa31e9460a
Vlad Avramenko <vladavramenko213@gmail.com>
2025-02-06 18:07:43 +0100

    Refactored script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
-console.log("Hello there, glad you've come to check my simple page. These are my first steps in making a webpage, so be my guest and change the page to your liking. For inquiries and suggestions contact me via e-mail: vladavramenko213@gmail.com. Cheers");
+{
+    const welcome = () => {
+        console.log("Hello there, glad you've come to check my simple page. These are my first steps in making a webpage, so be my guest and change the page to your liking. For inquiries and suggestions contact me via e-mail: vladavramenko213@gmail.com. Cheers");
+    }
 
-let themeToggle = document.querySelector(".theme-toggle");
-let content = document.querySelector(".content");
-let themeName = document.querySelector(".js-themeName");
+    welcome();
 
-themeToggle.addEventListener ("click", () => {
-    content.classList.toggle("content--dark-mode");
-    themeName.innerText = content.classList.contains ("content--dark-mode") ? "light" : "dark";
-});
+    const onThemeToggleClick = () => {
+        const content = document.querySelector(".content");
+        const themeName = document.querySelector(".js-themeName");
+        content.classList.toggle("content--dark-mode");
+        themeName.innerText = content.classList.contains("content--dark-mode") ? "light" : "dark";
+    }
 
-let button = document.querySelector(".button");
-let toggleInfo = document.querySelector(".js-toggleInfo");
-let contentVisible = document.querySelector(".content__container--visible");
+    const init = () => {
+        const themeToggle = document.querySelector(".theme-toggle");
+        themeToggle.addEventListener("click", onThemeToggleClick);
+    }
 
-button.addEventListener("click", () => {
-    contentVisible.classList.toggle("content__container--hidden");
-    toggleInfo.innerText = contentVisible.classList.contains ("content__container--hidden") ? "Pokaż" : "Ukryj";
-});
+    init();
+
+    const onToggleInfoClick = () => {
+        const toggleInfo = document.querySelector(".js-toggleInfo");
+        const contentVisible = document.querySelector(".content__container--visible");
+        contentVisible.classList.toggle("content__container--hidden");
+        toggleInfo.innerText = contentVisible.classList.contains("content__container--hidden") ? "Pokaż" : "Ukryj";
+    }
+
+   const toggleInfo = () => {
+    const button = document.querySelector(".button");
+    button.addEventListener("click", onToggleInfoClick);
+   }
+
+   toggleInfo();
+}
