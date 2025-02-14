9171fd83c65c74c11c65967ae52066b36de872cd
Vlad Avramenko <vladavramenko213@gmail.com>
2025-02-14 17:52:09 +0100

    adjusted margins for screens less than 500px width, refactored script.js to enhance readability of the code, deleted backup folder

--- js/script.js
+++ js/script.js
@@ -3,21 +3,12 @@
         console.log("Hello there, glad you've come to check my simple page. These are my first steps in making a webpage, so be my guest and change the page to your liking. For inquiries and suggestions contact me via e-mail: vladavramenko213@gmail.com. Cheers");
     }
 
-    welcome();
-
     const onThemeToggleClick = () => {
         const content = document.querySelector(".content");
         const themeName = document.querySelector(".js-themeName");
         content.classList.toggle("content--dark-mode");
         themeName.innerText = content.classList.contains("content--dark-mode") ? "light" : "dark";
     }
-
-    const init = () => {
-        const themeToggle = document.querySelector(".theme-toggle");
-        themeToggle.addEventListener("click", onThemeToggleClick);
-    }
-
-    init();
 
     const onToggleInfoClick = () => {
         const toggleInfo = document.querySelector(".js-toggleInfo");
@@ -26,10 +17,16 @@
         toggleInfo.innerText = contentVisible.classList.contains("content__container--hidden") ? "Pokaż" : "Ukryj";
     }
 
-   const toggleInfo = () => {
-    const button = document.querySelector(".button");
-    button.addEventListener("click", onToggleInfoClick);
-   }
+    const init = () => {
+        welcome();
 
-   toggleInfo();
+        const themeToggle = document.querySelector(".theme-toggle");
+        themeToggle.addEventListener("click", onThemeToggleClick);
+
+        const button = document.querySelector(".button");
+        button.addEventListener("click", onToggleInfoClick);
+    };
+
+    init();
+
 }
